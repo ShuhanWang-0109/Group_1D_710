b99f8615fed597d8f440cd81efb5dfc1e898fcbf
你的 GitHub 用户名 < GitHub >
2025-03-08 20:18:19 -0500

    Initial commit

--- Group_Project.R
+++ Group_Project.R
@@ -1,1 +1,5 @@
 #### Load the packages####
+
+library(readxl)
+urbanization <- read_excel("~/Public/Duke-2025/applied statistcal/Group_Projrct_Data/710_1D_Data/All_States_Urbanization_Trend.xlsx")
+View(urbanization)
