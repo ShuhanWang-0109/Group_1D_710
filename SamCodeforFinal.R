0e8a930f96da72c799ed2fa6f23071b44ada189c
samtduke <sam.tolbert@duke.edu>
2025-04-09 22:50:47 -0400

    Update SamCodeforFinal.R
    final edits

--- SamCodeforFinal.R
+++ SamCodeforFinal.R
@@ -9,162 +9,6 @@ library(lme4)
 library(ggplot2)
 
 
-#### Regression####
-
-# as.factor
-urbanization <- X0329_Data %>%
-  mutate(state = as.factor(state),
-         year = factor(year, ordered = TRUE),
-         rate = urban_area_km2/Population)
-# Remove the NAs
-urbanization_clean <- urbanization %>%
-  filter(!is.na(Bird_Count) & 
-           !is.nan(Bird_Count) & 
-           is.finite(Bird_Count) & 
-           !is.na(rate))  
-
-# linear model( bird data ~ urban/population  && bird data ~ time series )
-
-linear_model <- lm(Bird_Count ~ rate, data = urbanization_clean)
-
-summary(linear_model)
-
-
-linear_model_time<-lm(Bird_Count ~ year, data = urbanization_clean)
-linear_model_time
-
-# linear model visulization
-
-library(ggplot2)
-library(dplyr)  
-
-linear_plot_1 <- ggplot(urbanization_clean, aes(x = rate, y = Bird_Count)) +
-  geom_point(size = 0.8, alpha = 0.7) +  
-  geom_smooth(method = "lm", se = FALSE, color = "darkgreen", linewidth = 1.2) +  
-  labs(
-    x = "Urbanization (Urban Area km²)/Population", 
-    y = "Bird Count", 
-    title = "Relationship Between Urbanization and Bird Count" ,
-    color = "state"
-  ) +
-  scale_color_viridis_d()+
-  theme_minimal() +  
-  theme(
-    panel.grid.major = element_line(color = "gray90", linewidth = 0.2),  
-    panel.grid.minor = element_blank(), 
-    plot.title = element_text(size = 14, hjust = 0.5, face = "bold"),  
-    axis.title.x = element_text(size = 12, margin = margin(t = 10)),  
-    axis.title.y = element_text(size = 12, margin = margin(r = 10)),  
-    axis.text = element_text(size = 10)  
-  )
-
-linear_plot_1
-
-# Liner plot---time series
-linear_plot_time <- ggplot(urbanization_clean, aes(x = year, y = Bird_Count)) +
-  geom_boxplot(
-    width = 0.3,        
-    alpha = 0.5,        
-    outlier.size = 0.8,  
-    color = "gray40",   
-    fill = "gray90"     
-  ) +
-  geom_point(
-    aes(color= state),
-    size = 0.8, 
-    alpha = 0.7,
-    position = position_jitter(width = 0.1)  
-  ) +  
-  geom_smooth(
-    method = "lm", 
-    se = FALSE, 
-    color = "darkgreen", 
-    linewidth = 1.2,
-    aes(group = 1)  
-  ) +  
-  labs(
-    x = "Year", 
-    y = "Bird Count", 
-    title = "Relationship Between Year and Bird Count",
-  ) +
-  scale_color_viridis_d(option = "viridis") +
-  theme_minimal() +  
-  theme(
-    panel.grid.major = element_line(color = "gray90", linewidth = 0.2),  
-    panel.grid.minor = element_blank(), 
-    plot.title = element_text(size = 14, hjust = 0.5, face = "bold"),  
-    axis.title.x = element_text(size = 12, margin = margin(t = 10)),  
-    axis.title.y = element_text(size = 12, margin = margin(r = 10)),  
-    axis.text = element_text(size = 10)  
-  )
-
-linear_plot_time
-
-
-#### multiple regression- multi-linear regression####
-
-multiple_model<-lm(Bird_Count ~ rate + Temperature + Precipitation,data = urbanization_clean)
-summary(multiple_model)
-
-# mix_effect_model
-mixed_model <- lmer(Bird_Count ~ rate + Temperature + Precipitation + (1 | year) +(1 | state), data = urbanization_clean)
-summary(mixed_model)
-
-
-####0330Visulization####
-library(ggplot2)
-
-# Random effect(State)
-random_effects <- ranef(mixed_model)$state
-random_effects$state <- rownames(random_effects)  
-
-ggplot(random_effects, aes(x = state, y = `(Intercept)`)) +
-  geom_point(size = 3, color = "steelblue") +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  labs(
-    title = "Random Intercepts by State",
-    x = "State",
-    y = "Deviation from Fixed Intercept"
-  ) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-fixed_effects <- fixef(mixed_model)
-fixed_effects_df <- data.frame(
-  Predictor = names(fixed_effects),
-  Estimate = fixed_effects,
-  stringsAsFactors = FALSE
-)
-
-# Fix effect(others, temp, precipitation)
-ggplot(fixed_effects_df[-1, ], aes(x = Predictor, y = Estimate)) +  
-  geom_bar(stat = "identity", fill = "skyblue") +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  labs(
-    title = "Fixed Effects (Slopes) of Predictors",
-    x = "Predictor",
-    y = "Estimate"
-  ) +
-  theme_minimal()
-
-urbanization_clean$fitted <- predict(mixed_model)
-
-ggplot(urbanization_clean, aes(x = fitted, y = Bird_Count, color = state)) +
-  geom_point(alpha = 0.6) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "red") +
-  labs(
-    title = "Observed vs. Fitted Bird Count",
-    x = "Fitted Values",
-    y = "Observed Bird Count",
-    color = "State"
-  ) +
-  theme_minimal()
-
-
-
-
-
 # Sam's Plot
 
 #initial look at US urbanization trend by state/territory
@@ -205,15 +49,9 @@ urbanization_plot_adjustedscale <- urbanization%>%
 print(urbanization_plot_adjustedscale)
 
 # Sam's Species Charts
-
----
-  title: "Final Project Sam Drafting"
-output: pdf_document
-date: "2025-03-30"
----
   
   
-  Setup
+  #Setup
 ```{r setup, include=FALSE}
 knitr::opts_chunk$set(echo = TRUE)
 
@@ -290,7 +128,7 @@ ggplot(SpeciesDataWrangled, aes(x = Urban_Area_Per_Capita, y = `Northern Pintail
 
 
 
-#Run Poisson Distribution
+#Poisson Distribution
 
 library(ggplot2)
 
@@ -332,6 +170,7 @@ ggplot(SpeciesDataSwainson, aes(x = Urban_Area_Per_Capita, y = `Swainson's Hawk`
   theme_minimal(base_size = 11)
 
 
+#making tibble to characerize states by regions
 state_regions <-tibble(
   state = c("California", "Oregon", "Washington", "Nevada", "Arizona", "New Mexico", "Colorado", "Utah", "Idaho", "Montana", "Wyoming", "Alaska", "Hawaii",
             "North Dakota", "South Dakota", "Nebraska", "Kansas", "Minnesota", "Iowa", "Missouri", "Wisconsin", "Illinois", "Indiana", "Michigan", "Ohio",
